b31750b37b0071af203b906d128b4d885c72e139
dbeccaria826 <beccdaniel@gmail.com>
2020-09-19 18:39:20 -0400

    toggleButton: toggle button background colors change when switching modes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,9 @@
 const body = document.querySelector('body');
-const themeSwitcher = document.querySelector('.theme-switcher');
+const themeText = document.querySelector('.themeSwitch__wrapper');
+
 
 const toggleTheme = () => {
-  if (body.classList.value === 'isLight') {
-    body.classList.remove('isLight');
-    body.classList.add('isDark');
-  } else if (body.classList.value === 'isDark') {
-    body.classList.remove('isDark');
-    body.classList.add('isLight');
-  }
+  
 };
 
 themeSwitcher.addEventListener('click', toggleTheme);
