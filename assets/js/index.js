9d772f97c147c0ac2871c895be3166b5bc664021
dbeccaria826 <beccdaniel@gmail.com>
2020-10-17 13:43:34 -0400

    moved colors out of layout/base style components into their own theme file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,24 @@
-const body = document.querySelector('body');
-const themeText = document.querySelector('.themeSwitch__wrapper');
-
-
-const toggleTheme = () => {
+(function () {
+  const body = document.querySelector('body');
+  const themeText = document.querySelector('.themeSwitch__wrapper>em');
+  const themeSwitcher = document.querySelector('input[type=checkbox]');
   
-};
-
-themeSwitcher.addEventListener('click', toggleTheme);
+  const toggleTheme = () => {
+    if (body.classList.value === 'isLight') {
+      body.classList.remove('isLight');
+      body.classList.add('isDark');
+    } else {
+      body.classList.remove('isDark');
+      body.classList.add('isLight');
+    }
+    
+    if (themeText.textContent === "Light Mode") {
+      themeText.textContent = "Dark Mode";
+    } else {
+      themeText.textContent = "Light Mode";
+    }
+    console.log(body.classList);
+  };
+  
+  themeSwitcher.addEventListener('click', toggleTheme);
+})()
